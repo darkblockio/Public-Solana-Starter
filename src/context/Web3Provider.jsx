fe229d10bdbed017bfa1a340fef915e6500035dc
Camilo Valencia <acvalencia.contact@gmail.com>
2022-10-04 11:50:04 -0500

    implement solana wallet adapter

--- src/context/Web3Provider.jsx
+++ src/context/Web3Provider.jsx
@@ -1,9 +1,11 @@
-import { useCallback, useEffect, useReducer } from "react"
+import { useEffect, useState } from "react"
 import { Web3Context } from "./Web3Context"
-// import { TezosToolkit } from '@taquito/taquito';
-// import { BeaconWallet } from '@taquito/beacon-wallet'
-import { ConnectionProvider, WalletProvider, useWallet } from '@solana/wallet-adapter-react'
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import {
+  ConnectionProvider,
+  WalletProvider,
+  useWallet,
+} from "@solana/wallet-adapter-react"
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"
 import {
   BraveWalletAdapter,
   CoinbaseWalletAdapter,
@@ -16,129 +18,48 @@ import {
   SolletExtensionWalletAdapter,
   SolletWalletAdapter,
   TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets'
-import { WalletModalProvider, useWalletModal } from '@solana/wallet-adapter-react-ui'
-import { clusterApiUrl } from '@solana/web3.js'
-require('@solana/wallet-adapter-react-ui/styles.css')
+} from "@solana/wallet-adapter-wallets"
+import {
+  WalletModalProvider,
+  useWalletModal,
+} from "@solana/wallet-adapter-react-ui"
+import { clusterApiUrl } from "@solana/web3.js"
+require("@solana/wallet-adapter-react-ui/styles.css")
 
-const INITIAL_STATE = {
-  tezos: {
-    address: null,
-    publicKey: null,
-    provider: null,
-    chainError: false,
-  },
-}
+const SolanaContext = ({ children }) => {
+  const [address, setAddress] = useState(null)
+  const walletAdapter = useWallet()
+  const { setVisible } = useWalletModal()
 
-function tezosReducer(state, action) {
-  switch (action.type) {
-    case 'SET_PROVIDER':
-      return {
-        ...state,
-        tezos: {
-          address: action.address,
-          publicKey: action.publicKey,
-          provider: action.provider,
-        },
-      }
-    case 'SET_ADDRESS':
-      return {
-        ...state,
-        tezos: {
-          address: action.address,
-          publicKey: action.publicKey,
-        },
-      }
-    case 'RESET_PROVIDER':
-      return INITIAL_STATE
-    case 'SET_CHAIN_ERROR':
-      return {
-        ...INITIAL_STATE,
-        tezos: {
-          chainError: true,
-        },
-      }
-    default:
-      throw new Error()
+  useEffect(() => {
+    if (walletAdapter.connected) {
+      console.log("setting address: ", walletAdapter.publicKey.toBase58())
+      setAddress(walletAdapter.publicKey.toBase58())
+    }
+  }, [walletAdapter.connected]) // eslint-disable-line
+
+  const disconnect = () => {
+    setAddress(null)
+    walletAdapter.disconnect()
   }
-}
 
-let Tezos = null
-let tezosWallet = null
+  const connect = () => {
+    setVisible(true)
+  }
 
-if (typeof window !== 'undefined') {
-  Tezos = new TezosToolkit('https://mainnet-tezos.giganode.io')
-  tezosWallet = new BeaconWallet({ name: 'darkblock.io' })
-  Tezos.setWalletProvider(tezosWallet)
+  useEffect(() => {
+    console.log("connecting...")
+    connect()
+  }, []) // eslint-disable-line
+
+  return (
+    <Web3Context.Provider value={{ address, connect, disconnect, walletAdapter }} >
+      { children }
+    </Web3Context.Provider>
+  )
 }
 
 export const Web3Provider = ({ children }) => {
-  // const [state, dispatch] = useReducer(tezosReducer, INITIAL_STATE)
-  // const { provider, address, publicKey, chainError } = state.tezos
-
-  // const connect = useCallback(async function() {
-  //   if (typeof window !== 'undefined') {
-  //     try {
-  //       let address = null
-  //       let publicKey = null
-  //       let provider = null
-  //       let permissions = null
-  //       const activeAccount = await tezosWallet.client.getActiveAccount()
-
-  //       if (!activeAccount) {
-  //         await tezosWallet.clearActiveAccount()
-  //         permissions = await tezosWallet.client.requestPermissions()
-  //         address = permissions.address
-  //         publicKey = permissions.publicKey
-  //         provider = tezosWallet
-  //       } else {
-  //         address = activeAccount.address
-  //         publicKey = activeAccount.publicKey
-  //         provider = tezosWallet
-  //       }
-
-  //       if (permissions || activeAccount) {
-  //         dispatch({
-  //           type: 'SET_PROVIDER',
-  //           provider: provider,
-  //           address: address,
-  //           publicKey: publicKey,
-  //           chainError: false,
-  //         })
-  //       } else {
-  //         dispatch({
-  //           type: 'SET_CHAIN_ERROR',
-  //           chainError: true,
-  //         })
-  //       }
-  //     } catch (error) {
-  //       // connect()
-  //     }
-  //   }
-  // }, [])
-
-  // const disconnect = useCallback(async function() {
-  //   if (typeof window !== 'undefined') {
-  //     window.localStorage.removeItem('beacon:sdk-secret-seed')
-  //     window.localStorage.removeItem('beacon:sdk_version')
-  //     window.localStorage.removeItem('beacon:active-account')
-  //     window.localStorage.removeItem('beacon:accounts')
-  //     window.localStorage.removeItem('beacon:postmessage-peers-dapp')
-
-  //     Tezos = new TezosToolkit('https://mainnet-tezos.giganode.io')
-  //     tezosWallet = new BeaconWallet({ name: 'darkblock.io' })
-  //     Tezos.setWalletProvider(tezosWallet)
-  //   }
-
-  //   dispatch({
-  //     type: 'RESET_PROVIDER',
-  //   })
-  // }, [])
-
-  // useEffect(() => {
-  //   connect()
-  // }, [])
-
   const network = WalletAdapterNetwork.Devnet
   const endpoint = clusterApiUrl(network)
   const wallets = [
@@ -156,14 +77,14 @@ export const Web3Provider = ({ children }) => {
   ]
 
   return (
-    <Web3Context.Provider value={{ address, publicKey, connect, disconnect, provider, chainError }}>
-      <ConnectionProvider endpoint={endpoint}>
-        <WalletProvider wallets={wallets} autoConnect>
-          <WalletModalProvider>
-            { children }
-          </WalletModalProvider>
-        </WalletProvider>
-      </ConnectionProvider>
-    </Web3Context.Provider>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <SolanaContext>
+            {children}
+          </SolanaContext>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
